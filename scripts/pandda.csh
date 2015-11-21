d4fbee5b3d864ab0705fdb00c8b85f26ef65bc40
Nicholas Pearce <nicholas.pearce@stx.ox.ac.uk>
2015-11-21 16:06:20 +0000

    Changed configure.py file to autogenerate pandda.csh/sh files

--- scripts/pandda.csh
+++
@@ -1,7 +0,0 @@
-
-# Setup for PANDDAs
-setenv PYTHONPATH ${PYTHONPATH}:$PANDDA_TOP/lib-python
-setenv PATH ${PATH}:$PANDDA_TOP/bin
-#setenv PATH ${PATH}:$PANDDA_TOP/scripts
-
-
--- scripts/pandda.sh
+++
@@ -1,7 +0,0 @@
-
-# Setup for PANDDAs
-export PYTHONPATH=${PYTHONPATH}:$PANDDA_TOP/lib-python
-export PATH=${PATH}:$PANDDA_TOP/bin
-#export PATH=${PATH}:$PANDDA_TOP/scripts
-
-
