proper scaling of maps, bug fixes for reference map generation

lib-python/Giant/Xray/Data/cluster.py
@@ -48,7 +48,8 @@ class crystalGroup(object):
         if   method == 'lcv': link_func = pairwise_lcv
         dist_mat = link_func(unit_cells=[c.unit_cell for c in crystals])
         link_mat = scipy.cluster.hierarchy.linkage(dist_mat, method='single', metric='euclidean')
-        clusters = scipy.cluster.hierarchy.fcluster(link_mat, t=cutoff, criterion='inconsistent')
+#        clusters = scipy.cluster.hierarchy.fcluster(link_mat, t=cutoff, criterion='inconsistent')
+        clusters = scipy.cluster.hierarchy.fcluster(link_mat, t=cutoff, criterion='distance')
         return [cls([crystals[idx] for idx in g]) for i_g,g in generate_group_idxs(clusters)]
 
 def dendrogram(fname, link_mat, labels=None, ylab=None, xlab=None, ylim=None, annotate_y_min=0.25):