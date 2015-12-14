0e6bec6f328db689ca1e3d3420d483ec1824da9e
Nicholas Pearce <nicholas.pearce@stx.ox.ac.uk>
2015-12-14 23:55:52 +0000

    Changes to not require running of edstats for structure_summary

--- lib-python/Giant/Structure/summary.py
+++ lib-python/Giant/Structure/summary.py
@@ -38,52 +38,40 @@ class structureSummary(object):
 
     def get_atom_group_summary(self, atom_group):
         return atomGroupSummary( atom_group = atom_group,
-                                 edstats_scores = self._get_atom_group_edstats_scores(atom_group = atom_group),
                                  global_b_factor_statistics = self.b_factors )
+    def get_edstats_scores(self, atom_group):
+        return self._get_atom_group_edstats_scores(atom_group=atom_group)
 
     def to_html(self, fname):
         write_html_summary(fname=fname, atom_group_summaries=[self.get_atom_group_summary(ag) for ag in self.pdb_hierarchy.atom_groups()])
 
 class atomGroupSummary(object):
 
-    def __init__(self, atom_group, edstats_scores, global_b_factor_statistics):
+    def __init__(self, atom_group, global_b_facs_stats=None):
 
         self.atom_group = atom_group
-        self.edstats_scores = edstats_scores
-        self.b_factors              = atom_group.atoms().extract_b()
-        self.b_factors_statistics   = basic_statistics(atom_group.atoms().extract_b())
-        self.b_factors_z            = global_b_factor_statistics.to_z_score(b_vals=atom_group.atoms().extract_b())
-        self.b_factors_z_statistics = basic_statistics(self.b_factors_z)
-#        self._global_b_factor_statistics = global_b_factor_statistics
+
+        # Meta
+        self.residue_class = iotbx.pdb.common_residue_names_get_class(atom_group.resname)
+
+        # B-Factors
+        self.b_facs         = atom_group.atoms().extract_b()
+        self.b_facs_stats   = basic_statistics(atom_group.atoms().extract_b())
+        # B-Factors Z-Statistics
+        if global_b_facs_stats is not None:
+            self.b_facs_z       = global_b_facs_stats.to_z_score(b_vals=atom_group.atoms().extract_b())
+            self.b_facs_z_stats = basic_statistics(self.b_factors_z)
+        else:
+            self.b_facs_z       = None
+            self.b_facs_z_stats = None
+
+        # Occupancies
+        self.occies         = atom_group.atoms().extract_occ()
+        self.occies_stats   = basic_statistics(self.occies)
+
+        # Coordinates
+        self.centroid       = atom_group.atoms().extract_xyz().mean()
 
     def to_html(self, fname):
         write_html_summary(fname=fname, atom_group_summaries=[self])
-
-#class atomGroupSummary(object):
-#
-#    def __init__(self, atom_group):
-#
-#        self.atom_group = atom_group
-#
-#        # Meta about atom group
-#        self.residue_class = iotbx.pdb.common_residue_names_get_class(atom_group.resname)
-#
-#        # Occupancy summary
-#        occ_stats = atom_group.atoms().extract_occ().min_max_mean()
-#        self.occ_min = occ_stats.min
-#        self.occ_max = occ_stats.max
-#        self.occ_mean = occ_stats.mean
-#
-#        # B-factor summary
-#        b_stats = atom_group.atoms().extract_b().min_max_mean()
-#        self.b_min = b_stats.min
-#        self.b_max = b_stats.max
-#        self.b_mean = b_stats.mean
-#
-#        # Coordinate summary
-#        xyz_coords = atom_group.atoms().extract_xyz()
-#        self.xyz_min = xyz_coords.min()
-#        self.xyz_max = xyz_coords.max()
-#        self.xyz_mean = xyz_coords.mean()
-
 
