462d60c10e4d9e7428a1dcefe22e0b89c3fba142
Nicholas Pearce <nicholas.pearce@stx.ox.ac.uk>
2016-02-29 16:34:19 +0000

    added pandda.clean. implemented pandda.migrate

---
+++ lib-python/PANDDAs/jiffies/pandda_clean.py
@@ -0,0 +1,189 @@
+import os, sys, copy, glob
+import shutil
+
+import libtbx.phil
+
+import numpy
+
+############################################################################
+
+master_phil = libtbx.phil.parse("""
+input {
+    pandda_dir = None
+        .help = 'Path to the pandda directory to export files from'
+        .type = str
+}
+
+level = *basic normal ruthless devastating
+    .type = choice
+    .multiple = False
+
+skip_interesting = True
+    .help = 'Clean interesting datasets as well?'
+    .type = bool
+
+verbose = False
+    .type = bool
+
+YES_I_AM_SURE = False
+    .type = bool
+
+""")
+
+blank_arg_prepend = {None:'pandda_dir='}
+
+help = """
+Cleaning Levels:
+BASIC       - Remove unnecessary files
+NORMAL      - Remove easily regenerated dataset files - re-run pandda to remake: quick
+RUTHLESS    - Remove almost all dataset files         - re-run pandda to remake: slow
+DEVASTATING - Remove almost everything                - re-run pandda to remake: very slow
+
+Running with DEVASTATING essentially requires the pandda to be run again from scratch.
+
+Modelled Stuctures (in the modelled_structures folders) are NEVER deleted by pandda.clean.
+Files in the "analyses" and "results_summaries" folders are NEVER deleted by pandda.clean.
+Directories are also NEVER deleted (only files are deleted).
+"""
+
+############################################################################
+
+def delete_with_glob(glob_str):
+    for file in sorted(glob.glob(glob_str)):
+        # I know that unlink is the same as remove...
+        if os.path.islink(file):
+            print 'Unlinking file: {}'.format(file)
+            os.unlink(file)
+        else:
+            print 'Deleting file: {}'.format(file)
+            os.remove(file)
+
+def clean_misc(top_dir, level=0):
+    print '============================================================>>>'
+    print 'Cleaning Miscellaneous:'
+
+    # Level 2 cleaning
+    if level >= 2:
+        delete_with_glob(os.path.join(top_dir, 'aligned_structures', '*.pdb'))
+
+def clean_datasets(top_dir, level=0, skip_interesting=True):
+    print '============================================================>>>'
+    print 'Cleaning Dataset Folders:'
+
+    for d_dir in sorted(glob.glob(os.path.join(top_dir, 'processed_datasets', '*'))):
+
+        d_name = os.path.basename(d_dir)
+        print '=======================================>>>'
+
+        if skip_interesting and os.path.exists(os.path.join(top_dir, 'interesting_datasets', d_name)):
+            print 'Not Cleaning Interesting Dataset: {}'.format(d_name)
+            continue
+        else:
+            print 'Cleaning folder: {}'.format(d_dir)
+
+        # Level 1 cleaning - clean maps
+        if level >= 1:
+            delete_with_glob(os.path.join(d_dir, '*.ccp4'))
+
+        # Level 2 cleaning - clean structures and pickles as well
+        if level >= 2:
+            delete_with_glob(os.path.join(d_dir, '*.csv'))
+            delete_with_glob(os.path.join(d_dir, '*.pdb'))
+            delete_with_glob(os.path.join(d_dir, '*.mtz'))
+            delete_with_glob(os.path.join(d_dir, 'pickles', '*.pickle'))
+            delete_with_glob(os.path.join(d_dir, 'output_images', '*.png'))
+            delete_with_glob(os.path.join(d_dir, 'ligand_files',  '*.png'))
+
+    return
+
+def clean_links(top_dir, level=0, skip_interesting=True):
+    print '============================================================>>>'
+    print 'Cleaning Soft Links:'
+
+    # Level 0 cleaning
+    if level >= 0:
+        delete_with_glob(os.path.join(top_dir, 'resolutions', '*'))
+
+    # Level 1 cleaning
+    if level >= 1:
+        pass
+
+    # Level 2 cleaning
+    if level >= 2:
+        delete_with_glob(os.path.join(top_dir, 'empty_directories', '*'))
+        if not skip_interesting:
+            delete_with_glob(os.path.join(top_dir, 'interesting_datasets', '*'))
+
+    return
+
+def clean_statistical_maps(top_dir, level=0):
+    print '============================================================>>>'
+    print 'Cleaning Statistical Maps:'
+
+    # Level 0 cleaning - clean skew and kurt maps
+    if level >= 0:
+        delete_with_glob(os.path.join(top_dir, 'statistical_maps', '*-skew_map.ccp4'))
+        delete_with_glob(os.path.join(top_dir, 'statistical_maps', '*-kurt_map.ccp4'))
+        delete_with_glob(os.path.join(top_dir, 'statistical_maps', '*-bimo_map.ccp4'))
+        delete_with_glob(os.path.join(top_dir, 'statistical_maps', '*-stds_map.ccp4'))
+
+    # Level 1 cleaning - clean mean and sadj maps
+    if level >= 1:
+        delete_with_glob(os.path.join(top_dir, 'statistical_maps', '*-mean_map.ccp4'))
+        delete_with_glob(os.path.join(top_dir, 'statistical_maps', '*-sadj_map.ccp4'))
+
+    return
+
+def clean_pickles(top_dir, level=0):
+    print '============================================================>>>'
+    print 'Cleaning Pickles:'
+
+    # Level 2 cleaning - clean meta
+    if level >= 2:
+        delete_with_glob(os.path.join(top_dir, 'pickled_panddas', 'dataset_meta.pickle'))
+
+    # Level 3 cleaning - delete everthing
+    if level >= 3:
+        delete_with_glob(os.path.join(top_dir, 'pickled_panddas', '*.pickle'))
+
+def run(params):
+
+    assert params.input.pandda_dir, 'Must specify pandda directory'
+
+    if params.level   == 'basic':
+        ferocity = 0
+    elif params.level == 'normal':
+        ferocity = 1
+    elif params.level == 'ruthless':
+        ferocity = 2
+    elif params.level == 'devastating':
+        ferocity = 3
+
+    if ferocity > 1 and (not params.YES_I_AM_SURE):
+        raise SystemExit('level set to {} -- are you sure? if yes, set YES_I_AM_SURE=True and re-run'.format(params.level))
+
+    print 'Cleaning Pandda in folder: {}'.format(params.input.pandda_dir)
+    print 'Running at Ferocity Level of {} ({})'.format(ferocity, params.level)
+
+    clean_datasets(
+        top_dir=params.input.pandda_dir,
+        level=ferocity,
+        skip_interesting=params.skip_interesting
+    )
+
+    clean_links(
+        top_dir=params.input.pandda_dir,
+        level=ferocity,
+        skip_interesting=params.skip_interesting
+    )
+
+    clean_statistical_maps(
+        top_dir=params.input.pandda_dir,
+        level=ferocity
+    )
+
+    clean_pickles(
+        top_dir=params.input.pandda_dir,
+        level=ferocity
+    )
+
---
+++ lib-python/PANDDAs/jiffies/pandda_migrate.py
@@ -0,0 +1,73 @@
+import os, sys, copy, glob
+import shutil
+
+import libtbx.phil
+
+import numpy
+
+############################################################################
+
+master_phil = libtbx.phil.parse("""
+input {
+    pandda_dir = './pandda'
+        .help = 'Path to the pandda directory to use as a template'
+        .type = str
+}
+output {
+    out_dir = './pandda-migrate'
+        .help = 'output folder name'
+        .type = str
+}
+
+verbose = False
+    .type = bool
+
+""")
+
+############################################################################
+
+def copy_directory_contents(in_dir, out_dir, regex='*'):
+
+    if isinstance(regex, str):
+        regex = [regex]
+    else:
+        assert isinstance(regex, list), 'regex must either be str or list of str'
+        for r in regex: assert isinstance(r, str), 'regex must either be str or list of str'
+
+    if not os.path.exists(out_dir): os.mkdir(out_dir)
+
+    for file_regex in regex:
+        for file_in in sorted(glob.glob(os.path.join(in_dir, file_regex))):
+            # Create output filename
+            file_out = os.path.join(out_dir, os.path.basename(file_in))
+            # Copy file
+            print 'Copying {} -> {}'.format(file_in, file_out)
+            shutil.copy(file_in, file_out)
+
+def migrate_pandda(params):
+
+    # Create output directory
+    os.mkdir(params.output.out_dir)
+
+    # Copy across reference structures (copying the file if it's a softlink)
+    copy_directory_contents(
+        in_dir  = os.path.join(params.input.pandda_dir, 'reference'),
+        out_dir = os.path.join(params.output.out_dir,   'reference'),
+        regex   = ['*.pdb', '*.mtz', '*.ccp4']
+    )
+
+    # Copy across pickles (copying the file if it's a softlink)
+    copy_directory_contents(
+        in_dir  = os.path.join(params.input.pandda_dir, 'pickled_panddas'),
+        out_dir = os.path.join(params.output.out_dir,   'pickled_panddas'),
+        regex   = ['reference*.pickle', 'statistical*.pickle']
+    )
+
+def run(params):
+
+    assert params.input.pandda_dir, 'Must specify pandda directory'
+    assert params.output.out_dir,    'Must specify output directory'
+    assert not os.path.exists(params.output.out_dir), 'Output directory already exists'
+
+    print 'Migrating Pandda from {} to {}'.format(params.input.pandda_dir, params.output.out_dir)
+    migrate_pandda(params)
