4d7508f30b20cfb04cb33cc57e933548ce35db8c
Nicholas Pearce <nicholas.pearce@stx.ox.ac.uk>
2016-04-13 14:10:50 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='panddas',
+      version='1.0',
+      decription='Multi-dataset crystallographic analyses',
+      author='Nicholas M Pearce',
+      author_email='nicholas.pearce.0@gmail.com',
+      package_dir={'':'lib-python'},
+      packages=['panddas','Bamboo','Giant'],
+      )
