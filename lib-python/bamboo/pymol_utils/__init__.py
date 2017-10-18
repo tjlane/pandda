674060113bf02b4ddbb30adcda5b504af4aa11af
Nicholas Pearce <nicholas.pearce.0@gmail.com>
2017-10-18 16:09:15 +0200

    rewrite pymol script-generation class. autobuild functions from command strings. add defaults. etc.

--- lib-python/bamboo/pymol_utils/__init__.py
+++ lib-python/bamboo/pymol_utils/__init__.py
@@ -1,8 +1,9 @@
-import os
+import os, copy
 import numpy
 
 from itertools import cycle
 
+from bamboo.common.command import CommandManager
 from bamboo.pymol_utils.shapes import *
 
 
@@ -21,7 +22,11 @@ class _PymolScript(object):
 
     _colours = PymolColourPalettes.set1
 
-    _styles = ['cartoon', 'sticks', 'ribbon', 'dots', 'spheres', 'surface', 'mesh']
+    _styles = ['lines', 'cartoon', 'sticks', 'ribbon', 'dots', 'spheres', 'surface', 'mesh', 'ellipsoids']
+
+    defaults = {'ray':     {'width':1024, 'height':786},
+                'zoom':    {'buffer':1.0, 'complete':0},
+               }
 
     def __init__(self, pretty_models=True, pretty_maps=True):
 
@@ -29,13 +34,41 @@ class _PymolScript(object):
         self.prev = None
 
         self.lines = []
-        self._add(self._cmd_import_pml)
-        self._add(self._cmd_import_cgo)
+        self._append(self._import_pml)
+        self._append(self._import_cgo)
 
         if pretty_models: self.pretty_models()
         if pretty_maps:   self.pretty_maps()
 
         self.colour_palette(self._colours)
+
+    @classmethod
+    def _build(cls):
+        """Dynamically build the functions for this class"""
+
+        for prefix in ['_cmd_']:
+            for attribute_name in dir(cls):
+                if not attribute_name.startswith(prefix):
+                    continue
+                func_name = attribute_name[len(prefix):]
+                if hasattr(cls, func_name):
+                    continue
+                func = cls._make_method(func_name=func_name, template_name=attribute_name)
+                setattr(cls, func_name, func)
+
+    @classmethod
+    def _make_method(cls, func_name, template_name):
+        """Build custom functions for template population"""
+
+        def _func(self, *args, **kw_args):
+            defaults = copy.copy(self.defaults.get(func_name, {}))
+            defaults.update(kw_args)
+            template = getattr(self, template_name)
+            cmd = self._format(template=template, *args, **defaults)
+            self._append(cmd)
+            return cmd
+
+        return _func
 
     # ----------------------------------------------------------------------- #
     # Access/Modify Object Properties
@@ -55,10 +88,17 @@ class _PymolScript(object):
     # Append to/write script
     # ----------------------------------------------------------------------- #
 
-    def _add(self, thing):
-        if isinstance(thing, list):     [self._add(t) for t in thing]
+    def _format(self, template, *args, **kw_args):
+        assert kw_args.get('style') in [None]+self._styles
+        return template.format(*args, **kw_args)
+
+    def _append(self, thing):
+        if isinstance(thing, list):     [self._append(t) for t in thing]
         elif isinstance(thing, str):    self.lines.append(thing)
         else: raise Exception('Invalid type: {}'.format(type(thing)))
+
+    def append(self, thing):
+        self._append(thing)
 
     def write_script(self, f_name, overwrite=False):
         if (overwrite is True) and os.path.exists(f_name):
@@ -67,25 +107,16 @@ class _PymolScript(object):
         assert f_name.endswith(self._file_type)
         with open(f_name, 'w') as fh:
             fh.write('\n'.join(self.lines)+'\n')
-
-    # ----------------------------------------------------------------------- #
-    # Miscellanous Commands
-    # ----------------------------------------------------------------------- #
-
-    def ray_trace(self, width=1024, height=786):
-        self._add(self._cmd_ray.format(width=width, height=height))
-
-    def quit(self):
-        self._add(self._cmd_quit)
+        return f_name
 
     # ----------------------------------------------------------------------- #
     # Modify the settings in PyMOL session
     # ----------------------------------------------------------------------- #
 
     def set(self, setting, *args, **kwargs):
-        self._add(self._cmd_set.format(setting=setting,
-                                       args=', '.join([repr(a) for a in args]),
-                                       kwargs=', '.join(['{}={}'.format(k, repr(kwargs[k])) for k in kwargs.keys()])))
+        self._append(self._man_set.format(setting = setting,
+                                          args    = ', '.join([repr(a) for a in args]),
+                                          kwargs  = ', '.join(['{}={}'.format(k, repr(kwargs[k])) for k in kwargs.keys()])))
 
     def set_normalise_maps(self, value=True):
         self.set("normalize_ccp4_maps", int(value))
@@ -103,17 +134,17 @@ class _PymolScript(object):
     def load_pdb(self, f_name, obj=None):
         """Load pdb file f_name"""
         if obj is None: obj = os.path.basename(os.path.splitext(f_name)[0])
-        self._add(self._cmd_load_basic.format(f_name=f_name, obj=obj))
+        self._append(self._load_basic.format(f_name=f_name, obj=obj))
         return obj
 
     def load_map(self, f_name, obj=None):
         if obj is None: obj = os.path.basename(os.path.splitext(f_name)[0])
-        self._add(self._cmd_load_basic.format(f_name=f_name, obj=obj))
+        self._append(self._load_basic.format(f_name=f_name, obj=obj))
         return obj
 
     def make_mesh(self, obj, mesh_suffix='.mesh', contour_level=1, colour=None):
         mesh_name = obj+mesh_suffix
-        self._add(self._cmd_isomesh.format(obj=mesh_name, map=obj, contour_level=contour_level))
+        self._append(self._isomesh.format(obj=mesh_name, map=obj, contour_level=contour_level))
         if colour is not None: self.colour(obj=mesh_name, colour=colour)
         return mesh_name
 
@@ -122,7 +153,7 @@ class _PymolScript(object):
     # ----------------------------------------------------------------------- #
 
     def add_shape(self, shape, obj):
-        self._add(shape.as_cmd(name=obj))
+        self._append(shape.as_cmd(name=obj))
 
     # ----------------------------------------------------------------------- #
     # Multi-command defaults
@@ -138,24 +169,25 @@ class _PymolScript(object):
     # Visualisation
     # ----------------------------------------------------------------------- #
 
-    def repr_as(self, obj, style='cartoon'):
-        assert style in self._styles
-        self._add(self._cmd_show_as.format(style=style, obj=obj))
-        return style
+    def repr_as(self, *args, **kw_args):
+        return self.show_as(*args, **kw_args)
 
-    def repr_show(self, obj, style='cartoon'):
-        assert style in self._styles
-        self._add(self._cmd_show.format(style=style, obj=obj))
-        return style
+    def repr_show(self,*args, **kw_args):
+        return self.show(*args, **kw_args)
 
-    def repr_hide(self, obj, style='everything'):
-        self._add(self._cmd_hide.format(style=style, obj=obj))
-        return style
+    def repr_hide(self, *args, **kw_args):
+        return self.hide(*args, **kw_args)
 
     def colour(self, obj, colour=None):
         if colour is None: colour = self.get_colour()
-        self._add(self._cmd_colour.format(colour=colour, obj=obj))
+        cmd = self._format(template=self._man_colour, colour=colour, obj=obj)
+        self._append(cmd)
         return colour
+
+    def colour_by_element(self, obj, carbon_colour='green'):
+        cmd = self._format(template=self._util_colour_by[carbon_colour], obj=obj)
+        self._append(cmd)
+        return obj
 
     # ----------------------------------------------------------------------- #
 
@@ -164,20 +196,43 @@ class PythonScript(_PymolScript):
 
     _file_type = '.py'
 
-    _cmd_import_pml = 'from pymol import cmd'
-    _cmd_import_cgo = 'from pymol.cgo import *'
+    _import_pml = 'from pymol import cmd, util'
+    _import_cgo = 'from pymol.cgo import *'
+
+    _load_basic = 'cmd.load("{f_name}","{obj}")'
+    _isomesh    = 'cmd.isomesh("{obj}", "{map}", {contour_level})'
+
+    _man_set        = 'cmd.set("{setting}", {args}, {kwargs})'
+    _man_colour     = 'cmd.color("{colour}", "{obj}")'
 
     _cmd_quit       = 'cmd.quit()'
-
     _cmd_ray        = 'cmd.ray({width},{height})'
-
-    _cmd_load_basic = 'cmd.load("{f_name}","{obj}")'
-    _cmd_isomesh    = 'cmd.isomesh("{obj}", "{map}", {contour_level})'
-
+    _cmd_select     = 'cmd.select("{obj}", "{selection}")'
     _cmd_show_as    = 'cmd.show_as("{style}", "{obj}")'
     _cmd_show       = 'cmd.show("{style}", "{obj}")'
     _cmd_hide       = 'cmd.hide("{style}", "{obj}")'
-    _cmd_colour     = 'cmd.color("{colour}", "{obj}")'
-    _cmd_set        = 'cmd.set("{setting}", {args}, {kwargs})'
+    _cmd_orient     = 'cmd.orient("{obj}")'
+    _cmd_zoom       = 'cmd.zoom("{obj}", buffer={buffer}, complete={complete})'
+    _cmd_png        = 'cmd.png("{f_name}")'
 
+    _util_colour_by = {'black'    : 'util.cbab("{obj}")',
+                       'green'    : 'util.cbag("{obj}")',
+                       'magenta'  : 'util.cbam("{obj}")',
+                       'purple'   : 'util.cbap("{obj}")',
+                       'white'    : 'util.cbaw("{obj}")',
+                       'cyan'     : 'util.cbac("{obj}")',
+                       'pink'     : 'util.cbak("{obj}")',
+                       'orange'   : 'util.cbao("{obj}")',
+                       'salmon'   : 'util.cbas("{obj}")',
+                       'yellow'   : 'util.cbay("{obj}")'}
 
+    @classmethod
+    def run(cls, script):
+        assert script.endswith(cls._file_type)
+        c = CommandManager('pymol')
+        c.add_command_line_arguments(['-k', '-q', '-c', '-Q', '-s', script[:-len(cls._file_type)]+'.log', '-r', script])
+        c.run()
+        return c
+
+# Make the custom functions for the class at runtime
+PythonScript._build()
