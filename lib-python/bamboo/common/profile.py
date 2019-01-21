ebe820ae46d3256896a2bfa618d4b3103f957cd3
Nicholas Pearce <nicholas.pearce.0@gmail.com>
2019-01-21 16:01:11 +0100

    typo in class

--- lib-python/bamboo/common/profile.py
+++ lib-python/bamboo/common/profile.py
@@ -4,7 +4,7 @@ class profile_code(object):
     def __init__(self):
         self.start()
 
-    def start():
+    def start(self):
         self.profiler = cProfile.Profile()
         self.profiler.enable()
 
@@ -12,7 +12,7 @@ class profile_code(object):
         self.profiler.disable()
         s = StringIO.StringIO()
         sortby = 'cumulative'
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps = pstats.Stats(self.profiler, stream=s).sort_stats(sortby)
         if print_stats:
             ps.print_stats()
             print s.getvalue()
