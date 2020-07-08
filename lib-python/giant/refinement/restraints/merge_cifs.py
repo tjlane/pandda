dd409930d6c804c2632dee08c714a6fe465581b2
Nicholas Pearce <nicholas.pearce.0@gmail.com>
2020-07-08 10:56:45 +0200

    fix merge_cifs and create merge_cifs script

--- lib-python/giant/refinement/restraints/merge_cifs.py
+++ lib-python/giant/refinement/restraints/merge_cifs.py
@@ -13,24 +13,32 @@ def merge_cif_libraries(incifs, outcif):
     assert len(incifs) > 1, "'incifs' must be two or more files!"
 
     current = incifs.pop(0)
+    logger.debug('Starting with {}'.format(current))
 
     to_be_deleted = []
 
     for additional in incifs:
 
+        logger.debug('Merging {}'.format(additional))
+
         # Create a file handle and path for the output
         temp_handle, temp_path = tempfile.mkstemp(suffix='.lib', prefix='libcheck_')
         to_be_deleted.append(temp_path)
 
+        logger.debug('Writing to {}'.format(temp_path))
+
         # Merge current and additional to temp_path
-        prog = CommandManager('libcheck')
-        prog.extend_args([
+        prog = Dispatcher('libcheck')
+        prog.extend_stdin([
             '_DOC N',
             '_FILE_L {!s}'.format(current),
             '_FILE_L2 {!s}'.format(additional),
             '_FILE_O {!s}'.format(temp_path.replace('.lib','')),
             '_END',
         ])
+
+        logger.debug(prog.as_string())
+
         prog.run()
 
         current = temp_path
