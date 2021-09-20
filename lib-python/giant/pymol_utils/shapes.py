c20f8974eb9c6a0abf1527830ea3089175f64b03
Nicholas Pearce <nicholas.pearce.0@gmail.com>
2021-09-20 15:26:28 +0200

    add cone and arrow to pymol shapes

--- lib-python/giant/pymol_utils/shapes.py
+++ lib-python/giant/pymol_utils/shapes.py
@@ -9,7 +9,6 @@ import collections
 
 class Pseudoatom(object):
 
-
     def __init__(self, 
         selection = None,
         name = None,
@@ -59,7 +58,6 @@ class Pseudoatom(object):
 
 class Shape(object):
 
-
     def draw(self, obj):
         print 'Loading %s to %s ' % (self.SHAPE, obj)
         exec(self.as_cmd(obj))
@@ -73,12 +71,13 @@ class Shape(object):
 
 class Cylinder(Shape):
 
-
     SHAPE = 'Cylinder'
 
     def __init__(self, start, end, colors=None, radius=None):
-        if (colors is None):    colors = [[1,0,0]]*2
-        if (radius is None):     radius  = 0.1
+        if (colors is None):
+            colors = [[1,0,0],[1,0,0]]
+        if (radius is None):
+            radius = 0.1
         self.start = start
         self.end = end
         self.colors = colors
@@ -97,11 +96,13 @@ class Cylinder(Shape):
 
 class Sphere(Shape):
     
-
     SHAPE = 'Sphere'
 
-    def __init__(self, centre, radius=None, color=[1,1,1]):
-        if (radius is None):    radius  = 1
+    def __init__(self, centre, radius=None, color=None):
+        if (color is None):
+            color = [1,1,1]
+        if (radius is None):
+            radius = 1
         self.centre = centre
         self.radius = radius
         self.color = color
@@ -117,8 +118,99 @@ class Sphere(Shape):
         return s
 
 
-class Alpha(Shape):
+class Cone(Shape):
 
+    SHAPE = 'Cone'
+
+    def __init__(self, start, end, colors=None, radius=None):
+        if (colors is None):
+            colors = [[1,0,0],[1,0,0]]
+        if (radius is None):
+            radius = 0.1
+        self.start = start
+        self.end = end
+        self.colors = colors
+        self.radius = radius
+
+    def as_string(self):
+        s = "["
+        s += "CONE," + " %.6g, %.6g, %.6g," % tuple(self.start)
+        s += " %.6g, %.6g, %.6g," % tuple(self.end)
+        s += " %.6g, 0.0," % self.radius
+        s += " %.6g, %.6g, %.6g," % tuple(self.colors[0])
+        s += " %.6g, %.6g, %.6g," % tuple(self.colors[1])
+        s += " 1.0, 1.0"
+        s += "]"
+        return s
+
+
+class Arrow(object):
+
+    SHAPE = 'Arrow'
+
+    def __init__(self, start, end, colors=None, radius=None, head_length_fraction=0.4, head_radius_fraction=1.5):
+        if (colors is None):
+            colors = [[1,0,0],[1,0,0]]
+        if (radius is None):
+            radius = 0.1
+        self.start = start
+        self.end = end
+        self.colors = colors
+        self.radius = radius
+        self.head_length_fraction = head_length_fraction
+        self.head_radius_fraction = head_radius_fraction
+
+        assert self.head_length_fraction >= 0.
+        assert self.head_length_fraction <= 1.
+        assert self.head_radius_fraction > 0.
+
+    def make_components(self):
+
+        body_length_fraction = (1.0 - self.head_length_fraction)
+
+        start = tuple(self.start)
+        end = tuple(self.end)
+        mid = tuple(
+            start[i] + body_length_fraction * (end[i] - start[i])
+            for i in range(3)
+            )
+
+        c_start = list(self.colors[0])
+        c_end = list(self.colors[1])
+        c_mid = [
+            c_start[i] + body_length_fraction * (c_end[i] - c_start[i])
+            for i in range(3)
+            ]
+
+        return [
+            Cylinder(
+                start = start,
+                end = mid,
+                radius = self.radius,
+                colors = [c_start, c_mid],
+                ),
+            Cone(
+                start = mid,
+                end = end,
+                radius = (self.head_radius_fraction * self.radius),
+                colors = [c_mid, c_end],
+                ),
+        ]
+
+    def as_cmd(self, obj, state=0):
+        return 'cmd.load_cgo(%s, "%s", state=%s)' % (self.as_string(), obj, state)
+
+    def as_string(self):
+        cpts = self.make_components()
+        s = '['
+        for c in cpts:
+            s += c.as_string().strip('[] ')
+            s += ','
+        s = s.rstrip(', ') + ']'
+        return s
+
+
+class Alpha(Shape):
 
     SHAPE = 'None'
 
