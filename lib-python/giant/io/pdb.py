0f55594e9f3dbfffff09b94b4c435413fcdd34e8
Nicholas Pearce <nicholas.pearce.0@gmail.com>
2021-10-20 11:11:59 +0200

    remove old functions and move code to score_model before deleted

--- lib-python/giant/io/pdb.py
+++ lib-python/giant/io/pdb.py
@@ -18,22 +18,6 @@ def strip_pdb_to_input(input_pdb, remove_ter=False, remove_end=False):
                                         remove_end = remove_end     )
     return iotbx.pdb.hierarchy.input(pdb_string=stripped)
 
-def strip_pdb_to_file(input_pdb, output_pdb=None, remove_ter=False, remove_end=False):
-    """Remove all ter cards from a pdb file. If no output_file is given, operation is performed in_place"""
-    if not output_pdb: output_pdb=input_pdb
-    stipped = strip_pdb_to_string( input_pdb  = input_pdb,
-                                        remove_ter = remove_ter,
-                                        remove_end = remove_end     )
-    with open(output_pdb, 'w') as fh: fh.write(stipped)
-
-def reset_pdb_to_file(input_pdb, output_pdb=None, remove_ter=False, remove_end=False):
-    """Removes all of the TER and END records and regenerates them and write to file"""
-    if not output_pdb: output_pdb = input_pdb
-    inp = strip_pdb_to_input(   input_pdb  = input_pdb,
-                                remove_ter = remove_ter,
-                                remove_end = remove_end   )
-    inp.hierarchy.write_pdb_file(file_name=output_pdb)
-
 def get_pdb_header(pdb_file):
     """Read the header of the pdb file (up to the atom records)"""
 
